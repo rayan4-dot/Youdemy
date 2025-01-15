96e85014ab504693c2c2dae2e5fa3809b1bf6817
rayan4-dot <rayanpro2023@gmail.com>
2025-01-15 20:59:58 +0100

    update database

--- youdemy.sql
+++ youdemy.sql
@@ -1,61 +1,32 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Jan 14, 2025 at 08:14 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.0.28
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `youdemy`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Categories`
---
 
 CREATE TABLE `Categories` (
   `category_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+)  
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Courses`
---
 
 CREATE TABLE `Courses` (
   `course_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text DEFAULT NULL,
-  `content` text DEFAULT NULL,
   `teacher_id` int(11) NOT NULL,
   `category_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `status` enum('published','draft') NOT NULL DEFAULT 'draft',
+  `isCompleted` enum('notCompleted','completed') DEFAULT 'notCompleted',
+  `featured_image` varchar(255) DEFAULT NULL,
+  `scheduled_date` datetime DEFAULT NULL,
+  `contenu` enum('video','document') DEFAULT NULL
+)  
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Enrollments`
---
+CREATE TABLE `course_tags` (
+  `course_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+)  
 
 CREATE TABLE `Enrollments` (
   `enrollment_id` int(11) NOT NULL,
@@ -63,25 +34,16 @@ CREATE TABLE `Enrollments` (
   `course_id` int(11) NOT NULL,
   `enrolled_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `progress` enum('not_started','in_progress','completed') DEFAULT 'not_started'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+)  
 
--- --------------------------------------------------------
 
---
--- Table structure for table `Tags`
---
 
 CREATE TABLE `Tags` (
   `tag_id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+)  
 
--- --------------------------------------------------------
-
---
--- Table structure for table `Users`
---
 
 CREATE TABLE `Users` (
   `user_id` int(11) NOT NULL,
@@ -91,102 +53,6 @@ CREATE TABLE `Users` (
   `role` enum('student','teacher','admin') NOT NULL,
   `status` enum('active','suspended','pending') DEFAULT 'active',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Categories`
---
-ALTER TABLE `Categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `Courses`
---
-ALTER TABLE `Courses`
-  ADD PRIMARY KEY (`course_id`),
-  ADD KEY `teacher_id` (`teacher_id`),
-  ADD KEY `category_id` (`category_id`);
-
---
--- Indexes for table `Enrollments`
---
-ALTER TABLE `Enrollments`
-  ADD PRIMARY KEY (`enrollment_id`),
-  ADD UNIQUE KEY `student_id` (`student_id`,`course_id`),
-  ADD KEY `course_id` (`course_id`);
-
---
--- Indexes for table `Tags`
---
-ALTER TABLE `Tags`
-  ADD PRIMARY KEY (`tag_id`),
-  ADD UNIQUE KEY `name` (`name`);
-
---
--- Indexes for table `Users`
---
-ALTER TABLE `Users`
-  ADD PRIMARY KEY (`user_id`),
-  ADD UNIQUE KEY `email` (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `Categories`
---
-ALTER TABLE `Categories`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Courses`
---
-ALTER TABLE `Courses`
-  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Enrollments`
---
-ALTER TABLE `Enrollments`
-  MODIFY `enrollment_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Tags`
---
-ALTER TABLE `Tags`
-  MODIFY `tag_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `Users`
---
-ALTER TABLE `Users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Courses`
---
-ALTER TABLE `Courses`
-  ADD CONSTRAINT `courses_ibfk_1` FOREIGN KEY (`teacher_id`) REFERENCES `Users` (`user_id`),
-  ADD CONSTRAINT `courses_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `Categories` (`category_id`);
-
---
--- Constraints for table `Enrollments`
---
-ALTER TABLE `Enrollments`
-  ADD CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `Users` (`user_id`),
-  ADD CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `Courses` (`course_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `profile_picture_url` varchar(255) DEFAULT NULL
+)  
