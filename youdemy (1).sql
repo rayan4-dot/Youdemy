3127c5e24604bbaf6f476e80991576496281d08e
Rayan Elguerdaoui <137046025+rayan4-dot@users.noreply.github.com>
2025-03-28 23:49:44 +0000

    Add files via upload

---
+++ youdemy (1).sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 29, 2025 at 12:48 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `youdemy`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Categories`
+--
+-- Error reading structure for table youdemy.Categories: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`Categories`
+-- Error reading data for table youdemy.Categories: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`Categories`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+-- Error reading structure for table youdemy.courses: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`courses`
+-- Error reading data for table youdemy.courses: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`courses`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_tags`
+--
+-- Error reading structure for table youdemy.course_tags: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`course_tags`
+-- Error reading data for table youdemy.course_tags: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`course_tags`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Enrollments`
+--
+-- Error reading structure for table youdemy.Enrollments: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`Enrollments`
+-- Error reading data for table youdemy.Enrollments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`Enrollments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tags`
+--
+-- Error reading structure for table youdemy.Tags: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`Tags`
+-- Error reading data for table youdemy.Tags: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`Tags`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Users`
+--
+-- Error reading structure for table youdemy.Users: #1142 - SHOW command denied to user ''@'localhost' for table `youdemy`.`Users`
+-- Error reading data for table youdemy.Users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `youdemy`.`Users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
